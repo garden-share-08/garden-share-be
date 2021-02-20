5b20b663b854ef79c301e9e1de79366264024406
Joshua Carey <jdcarey128@gmail.com>
2021-02-20 11:28:30 -0700

    Define and test createListing mutation
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Dani Coleman <danicoleman00@gmail.com>
    Co-authored-by: Garrett Cottrell <garrett.cottrell2116@gmail.com>

--- app/graphql/types/listing_type.rb
+++ app/graphql/types/listing_type.rb
@@ -1,14 +1,15 @@
 module Types
   class ListingType < Types::BaseObject
+    field :user, Types::UserType, null: false
     field :zip_code, String, null: false
     field :produce_name, String, null: false
     field :produce_type, String, null: false
     field :description, String, null: true
-    field :quantity, Float, null: true
+    field :quantity, Integer, null: false
     field :unit, String, null: false
-    field :date_harvested, GraphQL::Types::ISO8601DateTime, null: false
+    field :date_harvested, String, null: false
     field :status, String, null: false
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :created_at, String, null: false
+    field :updated_at, String, null: false
   end
 end
--- spec/factories/listings.rb
+++ spec/factories/listings.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     produce_type { Faker::Food.fruits }
     description { Faker::Food.description }
     quantity { rand(1..10) }
-    unit { Faker::Food.metric_measurement }
+    unit { 'lbs'}
     date_harvested { "2021-02-20 08:27:22" }
     status { "pending" }
   end
