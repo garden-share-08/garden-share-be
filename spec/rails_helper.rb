9341a25057cd5597e87bc058cc4a35f88cec2338
Joshua Carey <jdcarey128@gmail.com>
2021-02-19 17:52:53 -0700

    Migrate, test user and configure user factory

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,6 +37,11 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  # factory syntax
+  RSpec.configure do |config|
+    config.include FactoryBot::Syntax::Methods
+  end
+  
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
@@ -59,6 +64,7 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
