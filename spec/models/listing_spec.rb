887e984e5a1922650e80facd74febdfdf150b25f
Dani Coleman <danicoleman00@gmail.com>
2021-02-20 08:44:34 -0700

    Add validations and relationship
    Co-authored-by: Joshua Carey <jdcarey128@gmail.com>
    Co-authored-by: Chris Allbritton 
    <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Garrett Cottrell <garrett.cottrell2116@gmail.com>

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Listing, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :zip_code }
+  it { should validate_presence_of :produce_name }
+  it { should validate_presence_of :produce_type }
+  it { should validate_presence_of :quantity }
+  it { should validate_presence_of :unit }
+  it { should validate_presence_of :date_harvested }
+
+  it { should belong_to :user }
 end
