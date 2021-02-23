7f517b2f3c657862740de8c8fde19edb704068d2
Dani Coleman <danicoleman00@gmail.com>
2021-02-23 14:36:35 -0700

    Add sad path test for hitting the error block with and invalid offer id
    Co-Authored-By: Callbritton <Callbritton@users.noreply.github.com>
    Co-Authored-By: Joshua Carey <jdcarey128@users.noreply.github.com>

--- spec/requests/mutations/accept_offer_spec.rb
+++ spec/requests/mutations/accept_offer_spec.rb
@@ -33,5 +33,40 @@ RSpec.describe 'backend acceptOffer mutation request' do
 
     expect(updated_listing[:status]).to eq("accepted")
     expect(updated_listing[:id]).to eq(listing.id)
+
+    error = result[:data][:acceptOffer][:error]
+    expect(error).to be_empty
+  end
+
+  it "errors out with an invalid offer id" do
+    seller = create(:user)
+    listing = create(:listing, user_id: seller.id)
+    buyer = create(:user)
+    offer = create(:offer, user_id: buyer.id, listing_id: listing.id)
+    query_string = <<-GRAPHQL
+    mutation {
+      acceptOffer(input: {
+        id: #{offer.id + 1}}) {
+        listing {
+          id
+          status
+          offers {
+            id
+            status
+          }
+        }
+        error
+      }
+    }
+    GRAPHQL
+
+    post graphql_path, params: { query: query_string }
+    result = JSON.parse(response.body, symbolize_names: true)
+    updated_listing = result[:data][:acceptOffer][:listing]
+
+    expect(updated_listing).to be_nil
+
+    error = result[:data][:acceptOffer][:error]
+    expect(error[0]).to eq("Couldn't find Offer with 'id'=#{offer.id + 1}")
   end
 end
