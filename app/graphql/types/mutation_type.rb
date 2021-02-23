209bbacc6ed50c63ecc446082893886330e842f6
Dani Coleman <danicoleman00@gmail.com>
2021-02-22 18:06:06 -0700

    Add accpet_offer field
    Co-Authored-By: Callbritton <Callbritton@users.noreply.github.com>

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,6 +4,7 @@ module Types
     field :create_listing, mutation: Mutations::CreateListing
     field :update_listing, mutation: Mutations::UpdateListing
     field :create_offer, mutation: Mutations::CreateOffer
-    field :delete_listing, mutation: Mutations::DeleteListing 
+    field :delete_listing, mutation: Mutations::DeleteListing
+    field :accept_offer, mutation: Mutations::AcceptOffer
   end
 end
